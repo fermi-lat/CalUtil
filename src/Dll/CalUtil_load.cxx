6a59cbe7e21f3bc28beca0a62b25d72c5d3df0d6
richard <>
2002-11-16 22:31:30 +0000

    for converter tool

--- src/Dll/CalUtil_load.cxx
+++ src/Dll/CalUtil_load.cxx
@@ -3,15 +3,20 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalDigi/src/Dll/CalDigi_load.cxx,v 1.3 2002/10/08 22:13:15 kyoung Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalUtil/src/Dll/CalUtil_load.cxx,v 1.1.1.1 2002/10/30 18:13:51 richard Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IToolFactory.h"
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 #define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 
 DECLARE_FACTORY_ENTRIES(CalUtil) {
     DECLARE_SERVICE( CalFailureModeSvc);
+    DLL_DECL_TOOL( LinearConvertAdc   );
 } 
+
 
