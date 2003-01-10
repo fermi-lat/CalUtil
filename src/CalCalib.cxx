3b955e127423664be3a26d7cb4ea8608ec405f33
chehtman <>
2003-01-10 23:28:27 +0000

    Calibration classes moved to CalUtil package

---
+++ src/CalCalib.cxx
@@ -0,0 +1,5 @@
+#include "CalUtil/CalCalibMap.h"
+#include "CalUtil/CalPedCalib.h"
+
+template<> class CalXtalCalib<CalPedCalib,CalPedElement,2>;
+template<> class CalCalibMap<CalPedCalib,CalPedElement,2>;
