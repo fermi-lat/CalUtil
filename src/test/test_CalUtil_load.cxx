e8d3314dea9a09f335ced53fc19575406b26694b
fewtrell <>
2005-02-05 09:26:53 +0000

    moving CalResponse to it's own package removed ConvertADC (obsolete) cleaned up requirements file took doxygen out of CVS (duh!) added CalDefs

--- src/test/test_CalUtil_load.cxx
+++ src/test/test_CalUtil_load.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalDigi/src/test/test_CalDigi_load.cxx,v 1.1 2002/08/18 16:01:56 richard Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalUtil/src/test/test_CalUtil_load.cxx,v 1.1.1.1 2002/10/30 18:13:51 richard Exp $
 //====================================================================
 //
 //  Description: Implementation of <Package>_load routine.
@@ -7,16 +7,9 @@
 //
 //====================================================================
 
-#include "GaudiKernel/ICnvFactory.h"
-#include "GaudiKernel/ISvcFactory.h"
 #include "GaudiKernel/IAlgFactory.h"
-
-
-#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& x##Factory; x##Factory.addRef();
 #define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
 #define DLL_DECL_OBJECT(x)     extern const IFactory& x##Factory; x##Factory.addRef();
-
 //! Load all  services: 
 void test_CalUtil_load() {
     DLL_DECL_ALGORITHM( test_CalUtil );
