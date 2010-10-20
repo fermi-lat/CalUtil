be75777eea5fbf08a4802923872218b0650b354a
jrb <>
2010-10-20 18:13:18 +0000

    add dependence on gaudi

--- CalUtilLib.py
+++ CalUtilLib.py
@@ -1,7 +1,8 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalUtil/CalUtilLib.py,v 1.2 2008/09/25 21:46:27 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalUtil/CalUtilLib.py,v 1.3 2008/11/12 19:39:28 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalUtil'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('xmlBaseLib')
