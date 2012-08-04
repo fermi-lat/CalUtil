4523b6fdf4c427f57744a43028f80f93d73ecdb7
lbaldini <>
2012-08-04 13:56:34 +0000

    EventLib added for the new CAL tools.

--- CalUtilLib.py
+++ CalUtilLib.py
@@ -1,4 +1,4 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalUtil/CalUtilLib.py,v 1.3 2008/11/12 19:39:28 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/CalUtil/CalUtilLib.py,v 1.4 2011/11/12 00:46:41 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalUtil'])
@@ -6,5 +6,7 @@ def generate(env, **kw):
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('xmlBaseLib')
+    env.Tool('EventLib')
+
 def exists(env):
     return 1;
