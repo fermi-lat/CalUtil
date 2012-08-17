e7d07f143ee98a0ce5870a77cf45eae2b4d8e473
jrb <>
2012-08-17 19:41:19 +0000

    patches for Windows no-install-headers

--- CalUtilLib.py
+++ CalUtilLib.py
@@ -1,12 +1,15 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/CalUtil/CalUtilLib.py,v 1.4 2011/11/12 00:46:41 jrb Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalUtil/CalUtilLib.py,v 1.4 2011/11/12 00:46:41 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalUtil'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'CalUtil') 
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['gaudiLibs'])
-    env.Tool('xmlBaseLib')
+    env.Tool('xmlBaseLib')    
     env.Tool('EventLib')
-
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'GlastSvc') 
 def exists(env):
     return 1;
